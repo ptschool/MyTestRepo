9c7e1f4dcc90cd3026c7ae65ce7236564bcc9532
Polat Parhat <polat@primetechschool.com>
2023-02-23 21:39:47 -0500

    Finish my work

--- test.java
+++ test.java
@@ -1,7 +1,8 @@
 public class test{
 
 	public static void main(String[] args){
-		System.out.println("Changes From SDET-2");
+		System.out.println("Changes From dev1");
+		System.out.println("more changes from dev1")
 	}
 
 
